0601d8c5c7ef303f1fca582d016f50a2f91d850c
maclend <maclen2005@wp.pl>
2026-04-22 00:12:31 +0200

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,3 +1,9 @@
+--Queries for F1 2025 Abu Dhabi SQL analysis project
+--Divided into categories:
+-- -lap analysis
+-- -qualifying results
+-- -race results
+
 -- FASTEST LAP
 SELECT driver, MIN(lap_time) FROM f1_lap_times
 GROUP BY driver
