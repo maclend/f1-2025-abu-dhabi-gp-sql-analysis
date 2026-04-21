16dcb9815b3592c66ed1ee337b27a8c27df59cc8
maclend <maclen2005@wp.pl>
2026-04-22 00:20:07 +0200

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -5,7 +5,8 @@
 -- -race results
 
 -- FASTEST LAP
-SELECT driver, MIN(lap_time) FROM f1_lap_times
+SELECT driver, MIN(lap_time) 
+FROM f1_lap_times
 GROUP BY driver
 ORDER BY MIN(lap_time)
 LIMIT 1;
