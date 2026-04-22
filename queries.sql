21f5b66a84ef14317759c275d1987d7c93d60834
maclend <maclen2005@wp.pl>
2026-04-22 21:18:01 +0200

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -2,7 +2,7 @@
 --Divided into categories:
 -- -lap analysis
 -- -qualifying session analysis
--- -race results
+-- -race results analysis
 
 -- 1) Lap analysis
 
@@ -25,7 +25,7 @@ FROM avg_times
 ORDER BY avg_lap_time;
 
 
---Fastest laps by each lap
+-- Fastest laps per lap
 WITH laps AS(
 SELECT lap_number, driver, 
 ROW_NUMBER() OVER(PARTITION BY lap_number ORDER BY lap_time) AS rnk
@@ -62,10 +62,40 @@ FROM S3
 WHERE rnk = 1;
 
 
+-- Fastest laps by each driver
+WITH laps AS(
+SELECT driver, lap_number, lap_time,
+ROW_NUMBER() OVER(PARTITION BY driver ORDER BY lap_time) AS rnk
+FROM f1_lap_times)
+SELECT full_name, lap_number, lap_time
+FROM laps
+JOIN f1_race_results
+ON f1_race_results.abbreviation = laps.driver
+WHERE rnk = 1
+ORDER BY lap_time;
+
+
+-- Max Verstappen's lap-by-lap performance
+SELECT lap_number, lap_time, 
+lap_time - LAG(lap_time) OVER(ORDER BY lap_number) AS delta
+FROM f1_lap_times
+WHERE driver = 'VER'
+ORDER BY lap_number;
+
+
+-- Ideal lap combining best sectors of each driver
+SELECT full_name,
+MIN(sector1) + MIN(sector2) + MIN(sector3) AS ideal_lap
+FROM f1_lap_times
+JOIN f1_race_results
+ON f1_lap_times.driver = f1_race_results.abbreviation
+GROUP BY full_name
+ORDER BY ideal_lap;
+
 
 -- 2) Qualifying session analysis
 
---Fastest laps by each session
+-- Fastest laps by each session
 WITH Q1 AS(
 SELECT full_name, q1_time,
 ROW_NUMBER() OVER(ORDER BY q1_time) AS rnk
@@ -91,20 +121,20 @@ FROM Q3
 WHERE rnk = 1;
 
 
---Average lap time by each qualifying session
+-- Average lap time by each qualifying session
 SELECT AVG(q1_time) AS Q1_avg_lap_time, 
 AVG(q2_time) AS Q2_avg_lap_time,
 AVG(q3_time) AS Q3_avg_lap_time
 FROM f1_q_results;
 
 
---Average Q1 lap time by each team
+-- Average Q1 lap time by each team
 SELECT team, AVG(q1_time) AS average_time FROM f1_q_results
 GROUP BY team
 ORDER BY average_time;
 
 
---Worst driver in qualifying session by team
+-- Worst performing driver in qualifying session per team
 WITH Q_results AS(
 SELECT full_name, team,
 ROW_NUMBER() OVER(PARTITION BY team ORDER BY pos) AS rnk
@@ -114,3 +144,36 @@ SELECT full_name, team
 FROM Q_results 
 WHERE rnk = 2
 ORDER BY team;
+
+
+
+-- 3) Race results analysis
+
+-- Lapped vs not lapped drivers
+SELECT COUNT(CASE WHEN status = 'Lapped' THEN 1 END) AS Lapped,
+COUNT(CASE WHEN status = 'Finished' THEN 1 END) AS Not_lapped
+FROM f1_race_results;
+
+
+-- Points by each team
+SELECT team_name, SUM(points) AS points
+FROM f1_race_results
+GROUP BY team_name
+ORDER BY points DESC;
+
+
+-- Positions gained by each driver
+SELECT full_name,  (grid_pos - pos) AS pos_gained
+FROM f1_race_results
+ORDER BY pos_gained DESC;
+
+
+-- Better driver by each team
+WITH race AS(
+SELECT team_name, full_name,
+ROW_NUMBER() OVER(PARTITION BY team_name ORDER BY pos) AS rnk
+FROM f1_race_results
+)
+SELECT team_name, full_name
+FROM race
+WHERE rnk = 1;
